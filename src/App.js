39570487a7d9e7a34974aa81144f1fd9e55b24b0
poms313 <pommine@free.fr>
2021-12-16 18:57:41 +0100

    create basic html and update package

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import React from 'react'
 import './App.css';
+import man from './assets/man.svg';
+import woman from './assets/woman.svg';
+import skin from './assets/skin.svg';
+import tshirt from './assets/t-shirt.svg';
+import light from './assets/color-FCD7B8.svg';
+import pink from './assets/color-E0A39A.svg';
+import yellow from './assets/color-FCCC84.svg';
+import darkBrown from './assets/color-533724.svg';
+import lightBlue from './assets/color-B2C7C7.svg';
+import green from './assets/color-88C10F.svg';
+import red from './assets/color-FF1414.svg';
+import striped from './assets/color-striped.svg';
+
+
 
 const App = () => {
-    return <div id="temp-style"> Develop your test here </div>
+    return <div id='temp-style'>
+        <h1>Profil</h1>
+        <h2>Lequel de ces deux avatars préférez-vous?</h2>
+        <form>
+            <div id='avatars'>
+                <img id='man' src={man} alt='man avatar'></img>
+                <img id='woman' src={woman} alt='woman avatar'></img>
+            </div>
+            <hr />
+            <div id='skinColor'>
+                <img id='skin' src={skin} alt='skin choice'></img>
+                <img id='light' src={light} alt='light skin'></img>
+                <img id='pink' src={pink} alt='pink skin'></img>
+                <img id='yellow' src={yellow} alt='yellow skin'></img>
+                <img id='darkBrown' src={darkBrown} alt='dark brown skin'></img>
+            </div>
+            <div id='shirtChoose'>
+                <img id='tshirt' src={tshirt} alt='t-shirt choice'></img>
+                <img id='lightBlue' src={lightBlue} alt='light blue t-shirt'></img>
+                <img id='green' src={green} alt='green t-shirt'></img>
+                <img id='red' src={red} alt='red t-shirt'></img>
+                <img id='striped' src={striped} alt='striped t-shirt'></img>
+            </div>
+            <button>Valider</button>
+        </form>
+    </div>
 }
 
 export default App;
