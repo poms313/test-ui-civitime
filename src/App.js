5d6ba9409e0ed6f4083eb1452c5139f79ff13422
poms313 <pommine@free.fr>
2021-12-16 20:05:15 +0100

    start css

--- src/App.js
+++ src/App.js
@@ -17,30 +17,34 @@ import striped from './assets/color-striped.svg';
 
 const App = () => {
     return <div id='temp-style'>
-        <h1>Profil</h1>
-        <h2>Lequel de ces deux avatars préférez-vous?</h2>
-        <form>
-            <div id='avatars'>
-                <img id='man' src={man} alt='man avatar'></img>
-                <img id='woman' src={woman} alt='woman avatar'></img>
+        <div id="container">
+            <div id="header">
+                <h1>Profil</h1>
             </div>
-            <hr />
-            <div id='skinColor'>
-                <img id='skin' src={skin} alt='skin choice'></img>
-                <img id='light' src={light} alt='light skin'></img>
-                <img id='pink' src={pink} alt='pink skin'></img>
-                <img id='yellow' src={yellow} alt='yellow skin'></img>
-                <img id='darkBrown' src={darkBrown} alt='dark brown skin'></img>
-            </div>
-            <div id='shirtChoose'>
-                <img id='tshirt' src={tshirt} alt='t-shirt choice'></img>
-                <img id='lightBlue' src={lightBlue} alt='light blue t-shirt'></img>
-                <img id='green' src={green} alt='green t-shirt'></img>
-                <img id='red' src={red} alt='red t-shirt'></img>
-                <img id='striped' src={striped} alt='striped t-shirt'></img>
-            </div>
-            <button>Valider</button>
-        </form>
+            <p>Lequel de ces deux avatars préférez-vous?</p>
+            <form id="UserAvatarChoice">
+                <div id='avatars'>
+                    <img id='man' src={man} alt='man avatar'></img>
+                    <img id='woman' src={woman} alt='woman avatar'></img>
+                </div>
+                <hr />
+                <div id='skinColorChoice'>
+                    <img id='skin' src={skin} alt='skin choice'></img>
+                    <img id='light' src={light} alt='light skin'></img>
+                    <img id='pink' src={pink} alt='pink skin'></img>
+                    <img id='yellow' src={yellow} alt='yellow skin'></img>
+                    <img id='darkBrown' src={darkBrown} alt='dark brown skin'></img>
+                </div>
+                <div id='shirtColorChoice'>
+                    <img id='tshirt' src={tshirt} alt='t-shirt choice'></img>
+                    <img id='lightBlue' src={lightBlue} alt='light blue t-shirt'></img>
+                    <img id='green' src={green} alt='green t-shirt'></img>
+                    <img id='red' src={red} alt='red t-shirt'></img>
+                    <img id='striped' src={striped} alt='striped t-shirt'></img>
+                </div>
+                <button>Valider</button>
+            </form>
+        </div>
     </div>
 }
 
